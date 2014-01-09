e6a4ade05e7b2f3325131e1d31a96c5c77dac93b
Henry Chan <henry@kinwo.net>
2014-01-09 20:49:00 +1100

    First commit. Adding a README.

---
+++ SliderPuzzle/Model/SPTile.h
@@ -0,0 +1,13 @@
+//
+//  SPTile.h
+//  SliderPuzzle
+//
+//  Created by HENRY CHAN on 9/01/2014.
+//  Copyright (c) 2014 Kinwo.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPTile : NSObject
+
+@end
---
+++ SliderPuzzle/Model/SPTile.m
@@ -0,0 +1,13 @@
+//
+//  SPTile.m
+//  SliderPuzzle
+//
+//  Created by HENRY CHAN on 9/01/2014.
+//  Copyright (c) 2014 Kinwo.net. All rights reserved.
+//
+
+#import "SPTile.h"
+
+@implementation SPTile
+
+@end
