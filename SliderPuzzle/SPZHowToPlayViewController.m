4862c561dfd13543da198b005818a7a186828326
Henry Chan <henry@kinwo.net>
2014-08-06 15:11:58 +1000

    Update Tour The App images.

--- SliderPuzzle/SPZHowToPlayViewController.m
+++ SliderPuzzle/SPZHowToPlayViewController.m
@@ -61,20 +61,23 @@
     MYIntroductionPanel *panel1 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Touch Friendly" description:@"Tap, Swipe or Drag and Drop to move squares." image:[UIImage imageNamed:@"TourTheApp1"]];
     [self updatePanelUI:panel1];
     
-    MYIntroductionPanel *panel2 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Get Hints" description:@"Touch & hold on 'Peek' button to get hints." image:[UIImage imageNamed:@"TourTheApp1"]];
+    MYIntroductionPanel *panel2 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Get Hints" description:@"Touch & hold on 'Peek' button to get hints." image:[UIImage imageNamed:@"TourTheApp3"]];
     [self updatePanelUI:panel2];
     
-    MYIntroductionPanel *panel3 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Play all levels" description:@"Tap 'More' button to switch to new levels." image:[UIImage imageNamed:@"TourTheApp1"]];
+    MYIntroductionPanel *panel3 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Choose difficulties" description:@"Choose among Easy, Medium and Difficult." image:[UIImage imageNamed:@"TourTheApp2"]];
     [self updatePanelUI:panel3];
-
-    MYIntroductionPanel *panel4 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"No score but a timer." description:@"Yes. We don't count your scores but we show you the Timer!" image:[UIImage imageNamed:@"TourTheApp1"]];
-    [self updatePanelUI:panel4];
     
-    MYIntroductionPanel *panel5 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Ready?" description:@"Enjoy playing Squarez!" image:[UIImage imageNamed:@"TourTheApp1"]];
+    MYIntroductionPanel *panel4 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Play all levels" description:@"Tap 'More' button to switch to new levels." image:[UIImage imageNamed:@"TourTheApp4"]];
+    [self updatePanelUI:panel4];
+
+    MYIntroductionPanel *panel5 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"No score but a timer." description:@"Yes. We don't count your scores but we show you the Timer!" image:[UIImage imageNamed:@"TourTheApp5"]];
     [self updatePanelUI:panel5];
     
+    MYIntroductionPanel *panel6 = [[MYIntroductionPanel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) title:@"Ready?" description:@"Enjoy playing Squarez!" image:[UIImage imageNamed:@"TourTheApp6"]];
+    [self updatePanelUI:panel6];
+    
     //Add panels to an array
-    NSArray *panels = @[panel1, panel2, panel3, panel4, panel5];
+    NSArray *panels = @[panel1, panel2, panel3, panel4, panel5, panel6];
     
     //Create the introduction view and set its delegate
     self.introductionView = [[MYBlurIntroductionView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
